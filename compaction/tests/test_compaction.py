de9adec9959145af3f61efe46208b795ea02be5f
mcflugen <mcflugen@gmail.com>
2017-04-04 16:21:42 -0600

    Test load_config and file-based compaction.

--- compaction/tests/test_compaction.py
+++ compaction/tests/test_compaction.py
@@ -1,9 +1,14 @@
 """Unit tests for compaction."""
+import pandas
 import numpy as np
-from numpy.testing import assert_array_less, assert_array_equal
-from nose.tools import assert_almost_equal
+from numpy.testing import (assert_array_less, assert_array_equal,
+                           assert_array_almost_equal)
+from nose.tools import assert_almost_equal, assert_dict_equal, assert_is_none
+import yaml
+from six import StringIO
 
-from compaction import compact
+from compaction import compact, load_config
+from compaction.compaction import run_compaction
 
 
 def test_decreasing_porosity():
@@ -83,3 +88,54 @@ def test_void_is_air():
     phi_1 = compact(dz_0, phi_0, porosity_max=.5, rho_void=0.)
     phi_2 = compact(dz_0, phi_0, porosity_max=.5, rho_void=1000.)
     assert_array_less(phi_1[1:], phi_2[1:])
+
+
+def test_load_config_defaults():
+    """Test load_config without file name."""
+    config = load_config()
+    defaults = {
+        'c': 5e-8,
+        'porosity_min': 0.,
+        'porosity_max': 1.,
+        'rho_grain': 2650.,
+        'rho_void': 1000.,
+    }
+    assert_dict_equal(config, defaults)
+
+
+def test_load_config_from_file():
+    """Test config vars from a file."""
+    file_like = StringIO()
+    yaml.dump(dict(c=3.14), file_like)
+    file_like.seek(0)
+    config = load_config(file_like)
+
+    expected = {
+        'c': 3.14,
+        'porosity_min': 0.,
+        'porosity_max': 1.,
+        'rho_grain': 2650.,
+        'rho_void': 1000.,
+    }
+    assert_dict_equal(config, expected)
+
+
+def test_run():
+    """Test running compaction with file-like objects."""
+    dz_0 = np.full(100, 1.)
+    phi_0 = np.full(100, .5)
+    phi_1 = compact(dz_0, phi_0, porosity_max=.5)
+
+    input = StringIO()
+    output = StringIO()
+
+    df = pandas.DataFrame.from_items([('dz', dz_0), ('porosity', phi_0)])
+    df.to_csv(input, index=False, header=False)
+
+    input.seek(0)
+    run_compaction(input=input, output=output, porosity_max=.5)
+    output.seek(0)
+
+    data = pandas.read_csv(output, names=('dz', 'porosity'), dtype=float)
+
+    assert_array_almost_equal(data.porosity, phi_1)
