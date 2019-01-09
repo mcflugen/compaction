2d5f0696db6618e8a1aca20d9cb31b7c93682835
mcflugen <mcflugen@gmail.com>
2019-01-09 15:48:30 -0700

    Install versioneer.

--- compaction/__init__.py
+++ compaction/__init__.py
@@ -1,3 +1,7 @@
 from .compaction import compact, load_config
 
 __all__ = ['compact', 'load_config']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
+import versioneer
 
-setup(name='compaction',
-      version='0.1',
-      description='Compact sediment',
-      author='Eric Hutton',
-      author_email='huttone@colorado.edu',
-      url='http://csdms.colorado.edu',
-      install_requires=['scipy', 'pandas', 'pyyaml'],
-      setup_requires=[],
-      packages=find_packages(),
-      entry_points={
-          'console_scripts': [
-              'compact=compaction.cli:main',
-          ],
-      },
+
+setup(
+    name="compaction",
+    description="Compact sediment",
+    author="Eric Hutton",
+    author_email="huttone@colorado.edu",
+    url="http://csdms.colorado.edu",
+    install_requires=["scipy", "pandas", "pyyaml"],
+    setup_requires=[],
+    packages=find_packages(),
+    entry_points={"console_scripts": ["compact=compaction.cli:main"]},
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
