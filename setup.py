684957fe00f61f8904652c9a13b118f7ce2453d8
mcflugen <mcflugen@gmail.com>
2020-02-17 19:10:24 -0700

    add requirements.txt file

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ import versioneer
 setup(
     name="compaction",
     description="Compact sediment",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author="Eric Hutton",
     author_email="huttone@colorado.edu",
     url="http://csdms.colorado.edu",
@@ -15,12 +17,16 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Physics",
     ],
-    install_requires=["click", "landlab", "numpy", "pandas", "pyyaml", "scipy"],
+    install_requires=open("requirements.txt", "r").read().splitlines(),
     setup_requires=[],
     packages=find_packages(),
-    entry_points={"console_scripts": ["compact=compaction.cli:main"]},
+    entry_points={
+        "console_scripts": ["compact=compaction.cli:compact"],
+        "landlab.components": ["Compact=compaction.landlab:Compact"],
+    },
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
