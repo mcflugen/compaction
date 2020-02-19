287ea59e0a10946c7e999bbe36446045a0207eb2
Eric Hutton <mcflugen@users.noreply.github.com>
2020-02-19 12:33:06 -0700

    feat: add show, stage, and run subcommands to the CLI (#11)
    * add compact cli subcommands, run/show/setup
    
    * add tests for show/stage subcommands
    
    * import StringIO
    
    * print all messages to stderr, output to stdout
    
    * more tests for show subcommand
    
    * remove lint
    
    * better test if reading from/writing to stdin/stdout
    
    * test read from/write to stdin/stdout
    
    * test cli without any subcommands
    
    * remove line from coverage
    
    * split lines for testing output with Windows
    
    * compare stripped lines when writing to stdout

--- compaction/cli.py
+++ compaction/cli.py
@@ -1,15 +1,22 @@
+import pathlib
 import sys
+from functools import partial
+from io import StringIO
 from typing import Optional, TextIO
 
 import click
+import numpy as np  # type: ignore
 import pandas  # type: ignore
 import yaml
 
 from ._version import get_versions
-from .compaction import compact
+from .compaction import compact as _compact
 
 __version__ = get_versions()["version"]
 del get_versions
+
+out = partial(click.secho, bold=True, err=True)
+err = partial(click.secho, fg="red", err=True)
 
 
 def load_config(file: Optional[TextIO] = None):
@@ -34,8 +41,28 @@ def load_config(file: Optional[TextIO] = None):
         "rho_void": 1000.0,
     }
     if file is not None:
-        conf.update(yaml.load(file))
+        conf.update(yaml.safe_load(file))
     return conf
+
+
+def _contents_of_input_file(infile: str) -> str:
+    params = load_config()
+
+    def as_csv(data, header=None):
+        with StringIO() as fp:
+            np.savetxt(fp, data, header=header, delimiter=",", fmt="%.1f")
+            contents = fp.getvalue()
+        return contents
+
+    contents = {
+        "config": yaml.dump(params, default_flow_style=False),
+        "porosity": as_csv(
+            [[1.0, 0.4], [1.0, 0.4], [1.0, 0.2]],
+            header="Layer Thickness [m], Porosity [-]",
+        ),
+    }
+
+    return contents[infile]
 
 
 def run_compaction(
@@ -44,9 +71,9 @@ def run_compaction(
     src = src or sys.stdin
     dest = dest or sys.stdout
 
-    init = pandas.read_csv(src, names=("dz", "porosity"), dtype=float)
+    init = pandas.read_csv(src, names=("dz", "porosity"), dtype=float, comment="#")
 
-    porosity_new = compact(init.dz.values, init.porosity.values, **kwds)
+    porosity_new = _compact(init.dz.values, init.porosity.values, **kwds)
 
     dz_new = init.dz * (1 - init.porosity) / (1 - porosity_new)
 
@@ -54,25 +81,103 @@ def run_compaction(
     out.to_csv(dest, index=False, header=False)
 
 
-@click.command()
+@click.group()
+@click.version_option()
+def compact() -> None:
+    """Compact layers of sediment.
+
+    \b
+    Examples:
+
+      Create a folder with example input files,
+
+        $ compact setup compact-example
+
+      Run a simulation using the examples input files,
+
+        $ compact run compact-example/porosity.csv
+    """
+    pass  # pragma: no cover
+
+
+@compact.command()
 @click.version_option(version=__version__)
 @click.option("-v", "--verbose", is_flag=True, help="Emit status messages to stderr.")
 @click.option("--dry-run", is_flag=True, help="Do not actually run the model")
 @click.option(
-    "--config", default=None, type=click.File(mode="r"), help="Configuration file"
+    "--config",
+    type=click.Path(
+        exists=False, file_okay=True, dir_okay=False, readable=True, allow_dash=False
+    ),
+    default="config.yaml",
+    is_eager=True,
+    help="Read configuration from PATH.",
 )
 @click.argument("src", type=click.File(mode="r"))
 @click.argument("dest", default="-", type=click.File(mode="w"))
-def main(src: TextIO, dest: TextIO, config: TextIO, dry_run: bool, verbose: bool):
+def run(src: TextIO, dest: TextIO, config: str, dry_run: bool, verbose: bool) -> None:
+    """Run a simulation."""
+    try:
+        from_stdin = src.name == "<stdin>"
+    except AttributeError:
+        from_stdin = True
+    try:
+        from_stdout = dest.name == "<stdout>"
+    except AttributeError:
+        from_stdout = True
 
-    params = load_config(config)
+    config_path = pathlib.Path(config)
+    if from_stdin:
+        rundir = pathlib.Path(".")
+    else:
+        rundir = pathlib.Path(src.name).parent.resolve()
+
+    with open(rundir / config_path, "r") as fp:
+        params = load_config(fp)
+
     if verbose:
-        click.secho(yaml.dump(params, default_flow_style=False), err=True)
+        out(yaml.dump(params, default_flow_style=False))
 
     if dry_run:
-        click.secho("Nothing to do. 😴", err=True, fg="green")
+        out("Nothing to do. 😴")
     else:
         run_compaction(src, dest, **params)
 
-        click.secho("💥 Finished! 💥", err=True, fg="green")
-        click.secho("Output written to {0}".format(dest.name), err=True, fg="green")
+        out("💥 Finished! 💥")
+        out("Output written to {0}".format("<stdout>" if from_stdout else dest.name))
+
+    sys.exit(0)
+
+
+@compact.command()
+@click.argument(
+    "infile", type=click.Choice(["config", "porosity"]),
+)
+def show(infile: str) -> None:
+    """Show example input files."""
+    print(_contents_of_input_file(infile))
+
+
+@compact.command()
+@click.argument(
+    "dest", type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
+)
+def setup(dest: str) -> None:
+    """Setup a folder of input files for a simulation."""
+    folder = pathlib.Path(dest)
+
+    files = [pathlib.Path(fname) for fname in ["porosity.csv", "config.yaml"]]
+
+    existing_files = [folder / name for name in files if (folder / name).exists()]
+    if existing_files:
+        for name in existing_files:
+            err(
+                f"{name}: File exists. Either remove and then rerun or choose a different destination folder",
+            )
+    else:
+        for fname in files:
+            with open(folder / fname, "w") as fp:
+                print(_contents_of_input_file(fname.stem), file=fp)
+        print(str(folder / "porosity.csv"))
+
+    sys.exit(len(existing_files))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import filecmp
 import os
 
 import pandas  # type: ignore
 import pytest  # type: ignore
+import numpy as np  # type: ignore
 import yaml
 from click.testing import CliRunner
 from numpy.testing import assert_array_almost_equal  # type: ignore
@@ -10,38 +12,26 @@ from numpy.testing import assert_array_almost_equal  # type: ignore
 from compaction import cli, compact
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_command_line_interface():
     """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main, ["--help"])
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(cli.compact, ["--help"])
     assert result.exit_code == 0
 
-    result = runner.invoke(cli.main, ["--version"])
+    result = runner.invoke(cli.compact, ["--version"])
     assert result.exit_code == 0
-    assert "version" in result.output
+    assert "version" in result.stdout
+
+    result = runner.invoke(cli.compact)
+    assert result.exit_code == 0
+    assert "Compact layers of sediment" in result.stdout
 
 
 def test_dry_run(tmpdir, datadir):
     with tmpdir.as_cwd():
         runner = CliRunner()
         result = runner.invoke(
-            cli.main,
+            cli.run,
             [
                 "--dry-run",
                 "--config={0}".format(datadir / "config.yaml"),
@@ -58,7 +48,7 @@ def test_verbose(tmpdir, datadir):
     with tmpdir.as_cwd():
         runner = CliRunner()
         result = runner.invoke(
-            cli.main,
+            cli.run,
             [
                 "--verbose",
                 "--config={0}".format(datadir / "config.yaml"),
@@ -69,7 +59,7 @@ def test_verbose(tmpdir, datadir):
         assert result.exit_code == 0
         assert (tmpdir / "out.txt").exists()
 
-        conf = yaml.load(os.linesep.join(result.output.splitlines()[:5]))
+        conf = yaml.safe_load(os.linesep.join(result.output.splitlines()[:5]))
         assert isinstance(conf, dict)
 
 
@@ -79,9 +69,9 @@ def test_constant_porosity(tmpdir, datadir):
     )
     phi_expected = compact(data["dz"], data["porosity"], porosity_max=0.6)
     with tmpdir.as_cwd():
-        runner = CliRunner()
+        runner = CliRunner(mix_stderr=False)
         result = runner.invoke(
-            cli.main,
+            cli.run,
             [
                 "--config={0}".format(datadir / "config.yaml"),
                 str(datadir / "porosity_profile.txt"),
@@ -90,7 +80,137 @@ def test_constant_porosity(tmpdir, datadir):
         )
 
         assert result.exit_code == 0
-        assert "Output written to out.txt" in result.output
+        assert "Output written to out.txt" in result.stderr
         phi_actual = pandas.read_csv("out.txt", names=("dz", "porosity"), dtype=float)
 
     assert_array_almost_equal(phi_actual["porosity"], phi_expected)
+
+
+def test_run_from_stdin(tmpdir, datadir):
+    path_to_porosity = str(datadir / "porosity_profile.txt")
+    with open(path_to_porosity) as fp:
+        porosity_profile = fp.read()
+
+    runner = CliRunner(mix_stderr=False)
+
+    with tmpdir.as_cwd():
+        result = runner.invoke(
+            cli.run, [
+                "--config={0}".format(datadir / "config.yaml"),
+                path_to_porosity,
+                "expected.txt",
+            ],
+        )
+        assert result.exit_code == 0
+
+        result = runner.invoke(
+            cli.run,
+            ["--config={0}".format(datadir / "config.yaml"), "-", "actual.txt"],
+            input=porosity_profile,
+        )
+        assert result.exit_code == 0
+
+        assert filecmp.cmp("actual.txt", "expected.txt")
+
+
+def test_run_to_stdout(tmpdir, datadir):
+    path_to_porosity = str(datadir / "porosity_profile.txt")
+
+    runner = CliRunner(mix_stderr=False)
+
+    with tmpdir.as_cwd():
+        result = runner.invoke(
+            cli.run, [
+                "--config={0}".format(datadir / "config.yaml"),
+                path_to_porosity,
+                "expected.txt",
+            ],
+        )
+        assert result.exit_code == 0
+        with open("expected.txt") as fp:
+            expected = fp.read()
+        expected_lines = [line.strip() for line in expected.splitlines() if line]
+
+        runner = CliRunner(mix_stderr=False)
+        result = runner.invoke(
+            cli.run, [
+                "--config={0}".format(datadir / "config.yaml"),
+                path_to_porosity,
+            ],
+        )
+        assert result.exit_code == 0
+        actual_lines = [line.strip() for line in result.stdout.splitlines() if line]
+
+        assert actual_lines == expected_lines
+
+
+def test_setup(tmpdir):
+    with tmpdir.as_cwd():
+        runner = CliRunner()
+        result = runner.invoke(cli.setup, ["."])
+
+        assert result.exit_code == 0
+        assert (tmpdir / "config.yaml").exists()
+        assert (tmpdir / "porosity.csv").exists()
+
+
+@pytest.mark.parametrize(
+    "files", (("config.yaml",), ("porosity.csv",), ("config.yaml", "porosity.csv"))
+)
+def test_setup_with_existing_files(tmpdir, files):
+    runner = CliRunner(mix_stderr=False)
+    with tmpdir.as_cwd():
+        for name in files:
+            with open(name, "w"):
+                pass
+
+        result = runner.invoke(cli.setup, ["."])
+
+        assert result.exit_code == len(files)
+        assert result.stdout == ""
+        for name in files:
+            assert name in result.stderr
+
+
+def test_show(tmpdir):
+    with tmpdir.as_cwd():
+        result = CliRunner(mix_stderr=False).invoke(cli.show, ["config"])
+        assert result.exit_code == 0
+        with open("config.yaml", "w") as fp:
+            fp.write(result.stdout)
+
+        result = CliRunner(mix_stderr=False).invoke(cli.show, ["porosity"])
+        assert result.exit_code == 0
+        with open("porosity.csv", "w") as fp:
+            fp.write(result.stdout)
+
+        result = CliRunner(mix_stderr=False).invoke(cli.run, ["porosity.csv", "out.csv"])
+
+        assert (tmpdir / "out.csv").exists()
+        assert result.exit_code == 0
+
+
+def test_show_config(tmpdir):
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(cli.show, ["config"])
+
+    assert result.exit_code == 0
+
+    params = yaml.safe_load(result.stdout)
+    assert isinstance(params, dict)
+
+
+def test_show_porosity(tmpdir):
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(cli.show, ["porosity"])
+
+    assert result.exit_code == 0
+
+    with open(tmpdir / "porosity.csv", "w") as fp:
+        fp.write(result.stdout)
+
+    data = pandas.read_csv(
+        tmpdir / "porosity.csv", names=("dz", "porosity"), dtype=float, comment="#"
+    )
+    assert np.all(data.dz.values > 0.0)
+    assert np.all(data.porosity.values >= 0.0)
