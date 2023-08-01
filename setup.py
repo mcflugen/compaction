cb2cd29aaa9ee98b84a9d154a41d82f42431d222
mcflugen <mcflugen@gmail.com>
2023-08-01 14:27:15 -0600

    add extension and openmp flags

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-from setuptools import setup
+import os
 
-setup()
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=cythonize(
+        Extension(
+            "compaction._compaction",
+            ["src/compaction/_compaction.pyx"],
+            extra_compile_args=["-fopenmp"] if "WITH_OPENMP" in os.environ else [],
+            extra_link_args=["-fopenmp"] if "WITH_OPENMP" in os.environ else [],
+        ),
+        compiler_directives={"embedsignature": True, "language_level": 3},
+    )
+)
