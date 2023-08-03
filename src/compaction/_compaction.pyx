1aeb13b8889ee15efeb9601c0833be4d4bf93d3b
mcflugen <mcflugen@gmail.com>
2023-08-03 11:03:12 -0600

    clean up parallel implementation

--- src/compaction/_compaction.pyx
+++ src/compaction/_compaction.pyx
@@ -1,3 +1,5 @@
+import multiprocessing
+
 cimport cython
 cimport openmp
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
@@ -6,12 +8,13 @@ from cython.parallel cimport prange
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def cumsum_rows(
+def cumsum_rows_serial(
     cython.floating [:, :] values,
     cython.floating [:, :] out,
 ):
     cdef long n_rows = values.shape[0]
     cdef long n_cols = values.shape[1]
+    cdef long row, col
 
     for col in range(n_cols):
         out[0, col] = values[0, col]
@@ -24,50 +27,51 @@ def cumsum_rows(
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def cumsum_rows_par(
-    cython.floating [:, :] values,
-    cython.floating [:, :] out,
+    cython.numeric [:, :] values,
+    cython.numeric [:, :] out,
 ):
-    cdef long n_rows = values.shape[0]
-    cdef long n_cols = values.shape[1]
-    # cdef long rows_per_block = 2
-    # cdef long n_blocks = n_rows // rows_per_block
-    cdef long n_blocks  # = openmp.omp_get_num_threads()
-    cdef long rows_per_block
-    cdef long block
-    cdef long start, end
-    cdef long row, col
-    cdef long completed_blocks = 0
-    # cdef double *partial_sum = <double*>PyMem_Malloc(sizeof(double) * n_blocks * n_cols)
-    cdef double *partial_sum
+    cdef int n_rows = values.shape[0]
+    cdef int n_cols = values.shape[1]
+    cdef int n_processors = multiprocessing.cpu_count()
+    cdef int n_blocks = n_processors - 1
+    cdef int block
+    cdef int row, col
+    cdef cython.numeric *partial_sum
+    cdef int *offset
 
-    openmp.omp_set_dynamic(1)
-    n_blocks = openmp.omp_get_num_threads()
-    rows_per_block = n_rows // n_blocks
-    partial_sum = <double*>PyMem_Malloc(sizeof(double) * n_blocks * n_cols)
+    if n_blocks > n_rows:
+        n_blocks = n_rows
+
+    partial_sum = <cython.numeric*>PyMem_Malloc(sizeof(cython.numeric) * (n_blocks-1) * n_cols)
+    offset = <int*>PyMem_Malloc(sizeof(int) * (n_blocks + 1))
 
     try:
-        for block in range(n_blocks):
+        for block in range(n_blocks + 1):
+            offset[block] = int(block * n_rows / n_blocks)
+
+        for col in prange((n_blocks - 1) * n_cols, nogil=True, schedule="static"):
+            partial_sum[col] = 0
+
+        for block in prange(n_blocks, nogil=True, schedule="static"):
             for col in range(n_cols):
-                partial_sum[block * n_cols + col] = 0.0
+                out[offset[block], col] = values[offset[block], col]
 
-        while completed_blocks < n_blocks:
-
-            for block in prange(completed_blocks, n_blocks, nogil=True, schedule="static"):
-                start = block * rows_per_block
-                end = start + rows_per_block
-                if end > n_rows:
-                    end = n_rows
-
+            for row in range(offset[block] + 1, offset[block + 1]):
                 for col in range(n_cols):
-                    out[start, col] = values[start, col] + partial_sum[block * n_cols + col]
+                    out[row, col] = values[row, col] + out[row - 1, col]
 
-                for row in range(start + 1, end):
-                    for col in range(n_cols):
-                        out[row, col] = values[row, col] + out[row - 1, col]
+        for col in range(n_cols):
+            partial_sum[col] = out[offset[1] - 1, col]
 
+        for block in range(1, n_blocks - 1):
+            for col in range(n_cols):
+                partial_sum[block * n_cols + col] = partial_sum[(block - 1) * n_cols + col] + out[offset[block + 1] - 1, col]
+
+        for block in prange(1, n_blocks, nogil=True, schedule="static"):
+            for row in range(offset[block], offset[block + 1]):
                 for col in range(n_cols):
-                    partial_sum[block * n_cols + col] = out[end - 1, col]
+                    out[row, col] = out[row, col] + partial_sum[(block - 1) * n_cols + col]
 
-            completed_blocks = completed_blocks + 1
     finally:
+        PyMem_Free(offset)
         PyMem_Free(partial_sum)
